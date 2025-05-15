eae7cdaac8e225826f724b5f9d41313efeb7804c
Ranjit Adhikari <rj.adhikari26@gmail.com>
2025-05-14 20:53:08 -0400

    Need new things hi

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hi, My name is Ranjit Adhikari");
         System.out.println("Anil dai padna aaunu");
+        System.out.println("Sanchai xa?");
     }
 }
